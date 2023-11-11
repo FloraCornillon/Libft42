7a0c76c8cedb9332da360f83c2b2a6b9ccba975e
Flora42 <149252736+Flora42@users.noreply.github.com>
2023-11-11 15:03:20 -0500

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -1,43 +1,59 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fcornill <fcornill@student.42quebec>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/10 13:03:51 by fcornill          #+#    #+#             */
-/*   Updated: 2023/11/10 16:16:23 by fcornill         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-#include "libftprintf.h"
+#include <stdarg.h>
+#include <unistsd.h>
+#include <stdio.h>
+
+int	ft_myputchar(int c)
+{
+	return (write(1, &c, 1));
+}
+
+int	ft_myputstr(char *s)
+{
+	int	count;
+
+	count = 0;
+	while (*s)
+		count += write(1, s++, 1);
+	return (count);
+}
+
+int	ft_myputnbr(long n, base)
+
+int	ft_get_format(char param, va_list ap)
+{
+	int	count;
+
+	count = 0;
+	if (param == 'c')
+		count += ft_myputchar(va_arg(ap, int));
+	else if (param == 's')
+		count += ft_myputstr(va_arg(ap, char *));
+	else if (param == 'd')
+		count += ft_myputnbr((long)va_arg(ap, int), 10);
+	else if (param == 'x')
+		count += ft_myputnbr((long)va_arg(ap, int), 16);
+	else
+		count += write(1, &param, 1);
+	return (count);
+}
 
 int	ft_printf(const char *format, ...)
 {
-	va_list			ap;
-	unsigned int	count;
+	int	count;
+	va_list	ap;
 
 	count = 0;
 	va_start(ap, format);
 	while (*format)
 	{
 		if (*format == '%')
-		{
-				format++;
-			if (*format == 'c')
-				count +=ft_putchar(ap.c);
-			else if (*format == 's')
-				count	+=ft_putstr(ap.str);
-			else if (*format == 'i')
-				count +=ft_putnumber(ap.num);
-			else if (*format == '%')
-				count +=ft_puchar('%');
-			else
-				count +=ft_putchar(*format);
-		}
+			count += ft_get_format(*format++, ap);
 		else
-			count +=ft_putchar(*format);
+			count += write(1, format, 1);
 		format++;
 	}
 	va_end(ap);
-	return (count);	
+	return (count);
 }
+				
+	
